1e815df923cee57e79be0095191a9377b2cf07b7
Bjørnar <bwendelborgrisdalen@gmail.com>
2019-01-22 12:53:32 +0100

    Update helloWorld.go

--- helloWorld.go
+++ helloWorld.go
@@ -7,4 +7,5 @@ func main() {
 	fmt.Println("Remote working is pretty difficult.")
 	fmt.Println("But I'm getting the hang of it.")
 	fmt.Println("Both online and from the terminal.")
+	fmt.Println("Hanne gjorde en forandring")
 }
